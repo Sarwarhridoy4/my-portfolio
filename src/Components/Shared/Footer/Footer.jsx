8c9ccda3013e950678012f6add674af73f94fcac
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-12-10 11:27:55 +0600

    make bg color same for nav and footer

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className='bg-white dark:bg-gray-900'>
+    <footer className='bg-white shadow dark:bg-gray-800'>
       <div className='container flex flex-col items-center justify-between p-6 mx-auto space-y-4 sm:space-y-0 sm:flex-row'>
         <a
           href='/'
