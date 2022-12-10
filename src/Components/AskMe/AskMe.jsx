bcfc03923d213dd31b6162c4873d5a889f4b6b93
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-12-10 17:36:05 +0600

    Done Without form functionalities

--- src/Components/AskMe/AskMe.jsx
+++ src/Components/AskMe/AskMe.jsx
@@ -3,54 +3,40 @@ import React from "react";
 const AskMe = () => {
   return (
     <section
-      class='min-h-screen bg-cover '
-      style={{ 
-        backgroundImage: `url("https://images.unsplash.com/photo-1563986768609-322da13575f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80")` 
+      className='min-h-screen bg-cover '
+      style={{
+        backgroundImage: `url("https://images.unsplash.com/photo-1563986768609-322da13575f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80")`,
       }}
     >
-      <div class='flex flex-col min-h-screen bg-black/60'>
-        <div class='container flex flex-col flex-1 px-6 py-12 mx-auto'>
-          <div class='flex-1 lg:flex lg:items-center lg:-mx-6'>
-            <div class='text-white lg:w-1/2 lg:mx-6'>
-              <h1 class='text-3xl font-semibold capitalize lg:text-4xl'>
-                Ultimate design solution
+      <div className='flex flex-col min-h-screen bg-black/60'>
+        <div className='container flex flex-col flex-1 px-6 py-12 mx-auto'>
+          <div className='flex-1 lg:flex lg:items-center lg:-mx-6'>
+            <div className='text-white lg:w-1/2 lg:mx-6'>
+              <h1 className='text-3xl font-semibold capitalize lg:text-4xl'>
+                Ultimate Dev solution
               </h1>
 
-              <p class='max-w-xl mt-6'>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Voluptatem quo aliquid molestiae hic incidunt beatae placeat
-                accusantium! Alias ex quisquam ab tempora. Ratione autem
-                doloremque ducimus numquam doloribus, error sed.
+              <p className='max-w-xl mt-6'>
+                I am providing 100% production ready web application using React
+                js, different kind of CSS framework,Node Pakage,MongoDB,Node,Express.
               </p>
 
-              <button class='px-8 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50'>
+              <button className='px-8 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50'>
                 get in touch
               </button>
 
-              <div class='mt-6 md:mt-8'>
-                <h3 class='text-gray-300 '>Follow us</h3>
+              <div className='mt-6 md:mt-8'>
+                <h3 className='text-gray-300 '>Follow Me</h3>
 
-                <div class='flex mt-4 -mx-1.5 '>
+                <div className='flex mt-4 -mx-1.5 '>
                   <a
-                    class='mx-1.5 text-white transition-colors duration-300 transform hover:text-blue-500'
-                    href='#'
+                    className='mx-1.5 text-white transition-colors duration-300 transform hover:text-blue-500'
+                    href='https://www.linkedin.com/in/sarwar-hridoy4/'
+                    target='_blank'
+                    rel='noreferrer'
                   >
                     <svg
-                      class='w-10 h-10 fill-current'
-                      viewBox='0 0 24 24'
-                      fill='none'
-                      xmlns='http://www.w3.org/2000/svg'
-                    >
-                      <path d='M18.6668 6.67334C18.0002 7.00001 17.3468 7.13268 16.6668 7.33334C15.9195 6.49001 14.8115 6.44334 13.7468 6.84201C12.6822 7.24068 11.9848 8.21534 12.0002 9.33334V10C9.83683 10.0553 7.91016 9.07001 6.66683 7.33334C6.66683 7.33334 3.87883 12.2887 9.3335 14.6667C8.0855 15.498 6.84083 16.0587 5.3335 16C7.53883 17.202 9.94216 17.6153 12.0228 17.0113C14.4095 16.318 16.3708 14.5293 17.1235 11.85C17.348 11.0351 17.4595 10.1932 17.4548 9.34801C17.4535 9.18201 18.4615 7.50001 18.6668 6.67268V6.67334Z' />
-                    </svg>
-                  </a>
-
-                  <a
-                    class='mx-1.5 text-white transition-colors duration-300 transform hover:text-blue-500'
-                    href='#'
-                  >
-                    <svg
-                      class='w-8 h-8'
+                      className='w-8 h-8'
                       viewBox='0 0 24 24'
                       fill='none'
                       xmlns='http://www.w3.org/2000/svg'
@@ -71,11 +57,13 @@ const AskMe = () => {
                   </a>
 
                   <a
-                    class='mx-1.5 text-white transition-colors duration-300 transform hover:text-blue-500'
-                    href='#'
+                    className='mx-1.5 text-white transition-colors duration-300 transform hover:text-blue-500'
+                    href='https://www.facebook.com/sarwarhridoy4/'
+                    target='_blank'
+                    rel='noreferrer'
                   >
                     <svg
-                      class='w-8 h-8'
+                      className='w-8 h-8'
                       viewBox='0 0 24 24'
                       fill='none'
                       xmlns='http://www.w3.org/2000/svg'
@@ -88,11 +76,13 @@ const AskMe = () => {
                   </a>
 
                   <a
-                    class='mx-1.5 text-white transition-colors duration-300 transform hover:text-blue-500'
-                    href='#'
+                    className='mx-1.5 text-white transition-colors duration-300 transform hover:text-blue-500'
+                    href='https://www.instagram.com/sarwarhridoy04/'
+                    target='_blank'
+                    rel='noreferrer'
                   >
                     <svg
-                      class='w-8 h-8'
+                      className='w-8 h-8'
                       viewBox='0 0 24 24'
                       fill='none'
                       xmlns='http://www.w3.org/2000/svg'
@@ -107,51 +97,51 @@ const AskMe = () => {
               </div>
             </div>
 
-            <div class='mt-8 lg:w-1/2 lg:mx-6'>
-              <div class='w-full px-8 py-10 mx-auto overflow-hidden bg-white shadow-2xl rounded-xl dark:bg-gray-900 lg:max-w-xl'>
-                <h1 class='text-2xl font-medium text-gray-700 dark:text-gray-200'>
+            <div className='mt-8 lg:w-1/2 lg:mx-6'>
+              <div className='w-full px-8 py-10 mx-auto overflow-hidden bg-white shadow-2xl rounded-xl dark:bg-gray-900 lg:max-w-xl'>
+                <h1 className='text-2xl font-medium text-gray-700 dark:text-gray-200'>
                   Contact form
                 </h1>
 
-                <p class='mt-4 text-gray-500 dark:text-gray-400'>
-                  Ask us everything and we would love to hear from you
+                <p className='mt-4 text-gray-500 dark:text-gray-400'>
+                  Ask me anything and I would love to hear from you
                 </p>
 
-                <form class='mt-6'>
-                  <div class='flex-1'>
-                    <label class='block mb-2 text-sm text-gray-600 dark:text-gray-200'>
+                <form className='mt-6'>
+                  <div className='flex-1'>
+                    <label className='block mb-2 text-sm text-gray-600 dark:text-gray-200'>
                       Full Name
                     </label>
                     <input
                       type='text'
-                      placeholder='John Doe'
-                      class='block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring'
+                      placeholder='Youe Name'
+                      className='block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring'
                     />
                   </div>
 
-                  <div class='flex-1 mt-6'>
-                    <label class='block mb-2 text-sm text-gray-600 dark:text-gray-200'>
+                  <div className='flex-1 mt-6'>
+                    <label className='block mb-2 text-sm text-gray-600 dark:text-gray-200'>
                       Email address
                     </label>
                     <input
                       type='email'
-                      placeholder='johndoe@example.com'
-                      class='block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring'
+                      placeholder='Your Email'
+                      className='block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring'
                     />
                   </div>
 
-                  <div class='w-full mt-6'>
-                    <label class='block mb-2 text-sm text-gray-600 dark:text-gray-200'>
+                  <div className='w-full mt-6'>
+                    <label className='block mb-2 text-sm text-gray-600 dark:text-gray-200'>
                       Message
                     </label>
                     <textarea
-                      class='block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring'
-                      placeholder='Message'
+                      className='block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring'
+                      placeholder='How Can I Help you?'
                     ></textarea>
                   </div>
 
-                  <button class='w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50'>
-                    get in touch
+                  <button className='w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50'>
+                    Submit
                   </button>
                 </form>
               </div>
--- src/Components/Home/MyPortfolio/MyPortfolio.jsx
+++ src/Components/Home/MyPortfolio/MyPortfolio.jsx
@@ -1,60 +1,83 @@
-import React from 'react';
+import React from "react";
 
 const MyPortfolio = () => {
-    return (
-        <section class="bg-white dark:bg-gray-900">
-    <div class="container px-6 py-10 mx-auto">
-        <h1 class="text-3xl font-semibold text-center text-gray-800 capitalize lg:text-4xl dark:text-white">Portfolio
+  return (
+    <section className='bg-white dark:bg-gray-900'>
+      <div className='container px-6 py-10 mx-auto'>
+        <h1 className='text-3xl font-semibold text-center text-gray-800 capitalize lg:text-4xl dark:text-white'>
+          Portfolio
         </h1>
 
-        <p class="mt-4 text-center text-gray-500 dark:text-gray-300">
-            Some of my projects are here 
+        <p className='mt-4 text-center text-gray-500 dark:text-gray-300'>
+          Some of my projects are here
         </p>
 
-
-        <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
-            <div class="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
-                style={{ 
-                    backgroundImage: `url("https://i.ibb.co/CwPHk0d/1.png")` 
-                  }}>
-                <div
-                    class="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
-                    <h2 class="mt-4 text-2xl font-semibold text-white capitalize">Used Mobile Deal</h2>
-                    <a href='https://used-mobile-deal.web.app/' class="mt-2 text-lg tracking-wider text-blue-400 uppercase ">Website</a>
-                </div>
+        <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3'>
+          <div
+            className='overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group'
+            style={{
+              backgroundImage: `url("https://i.ibb.co/CwPHk0d/1.png")`,
+            }}
+          >
+            <div className='flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100'>
+              <h2 className='mt-4 text-2xl font-semibold text-white capitalize'>
+                Used Mobile Deal
+              </h2>
+              <a
+                href='https://used-mobile-deal.web.app/'
+                className='mt-2 text-lg tracking-wider text-blue-400 uppercase '
+              >
+                Website
+              </a>
             </div>
+          </div>
 
-            <div class="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
-                style={{ 
-                    backgroundImage: `url("https://i.ibb.co/g7smMYg/2.png")` 
-                  }}>
-                <div
-                    class="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
-                    <h2 class="mt-4 text-2xl font-semibold text-white capitalize">WebLogig Solutions</h2>
-                    <a href='https://weblogic-solution.web.app/' class="mt-2 text-lg tracking-wider text-blue-400 uppercase ">Website</a>
-                </div>
+          <div
+            className='overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group'
+            style={{
+              backgroundImage: `url("https://i.ibb.co/g7smMYg/2.png")`,
+            }}
+          >
+            <div className='flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100'>
+              <h2 className='mt-4 text-2xl font-semibold text-white capitalize'>
+                WebLogig Solutions
+              </h2>
+              <a
+                href='https://weblogic-solution.web.app/'
+                className='mt-2 text-lg tracking-wider text-blue-400 uppercase '
+              >
+                Website
+              </a>
             </div>
+          </div>
 
-            <div class="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
-                style={{ 
-                    backgroundImage: `url("https://i.ibb.co/wNp9P5t/3.png")` 
-                  }}>
-                <div
-                    class="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
-                    <h2 class="mt-4 text-2xl font-semibold text-white capitalize">E-PathShala</h2>
-                    <a href='https://e-pathshala-authentication.web.app/home' class="mt-2 text-lg tracking-wider text-blue-400 uppercase ">Website</a>
-                </div>
-                    </div>
-                    
+          <div
+            className='overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group'
+            style={{
+              backgroundImage: `url("https://i.ibb.co/wNp9P5t/3.png")`,
+            }}
+          >
+            <div className='flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100'>
+              <h2 className='mt-4 text-2xl font-semibold text-white capitalize'>
+                E-PathShala
+              </h2>
+              <a
+                href='https://e-pathshala-authentication.web.app/home'
+                className='mt-2 text-lg tracking-wider text-blue-400 uppercase '
+              >
+                Website
+              </a>
+            </div>
+          </div>
         </div>
-            </div>
-            <div className="flex items-center justify-center">
-                    <button class="mx-8 px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-                    More...
-                    </button>
-                    </div>
-</section>
-    );
+      </div>
+      <div className='flex items-center justify-center'>
+        <button className='mx-8 px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80'>
+          More...
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default MyPortfolio;
