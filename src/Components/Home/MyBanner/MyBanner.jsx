bb468c5ade1fd7f8e350215974fba88f22a4c94b
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-12-20 12:21:12 +0600

    cover photo change

--- src/Components/Home/MyBanner/MyBanner.jsx
+++ src/Components/Home/MyBanner/MyBanner.jsx
@@ -85,8 +85,8 @@ const MyBanner = () => {
 
         <div className='flex items-center justify-center w-full h-96 lg:w-1/2'>
           <img
-            className='object-cover w-full h-full mx-auto rounded-md lg:max-w-2xl'
-            src='https://images.unsplash.com/photo-1543269664-7eef42226a21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+            className='bg-cover bg-center w-full h-full mx-auto rounded-md lg:max-w-2xl'
+            src='https://i.ibb.co/ZK6XyM9/cover.gif'
             alt='glasses'
           />
         </div>
